472e9a82aec062a5ddb8da71a5420f32e7b9422b
john <john@john-unix>
2023-03-19 10:26:14 +0100

    task strategy added

--- src/main/java/com/tradebot/configuration/OrdersParams.java
+++ src/main/java/com/tradebot/configuration/OrdersParams.java
@@ -4,22 +4,13 @@ import com.tradebot.model.OrderSide;
 import java.util.LinkedHashMap;
 
 public class OrdersParams {
-
-//	public static LinkedHashMap<String, Object> getOrderBuyParams(String symbol, Integer quoteQty, long timeStamp) {
-//		return getParams("BUY", symbol, quoteQty, timeStamp);
-//	}
-//	
-//	public static LinkedHashMap<String, Object> getOrderSellParams(String symbol, Integer quoteQty, long timeStamp) {
-//		return getParams("SELL", symbol, quoteQty, timeStamp);
-//	}
 	
-	public static LinkedHashMap<String, Object> getParams(String symbol, OrderSide side, Integer quoteQty, long timeStamp){
+	public static LinkedHashMap<String, Object> getOrderParams(String symbol, OrderSide side, Integer quoteQty, long timeStamp){
 		
 		LinkedHashMap<String, Object> parameters = new LinkedHashMap<>();
 		parameters.put("symbol", symbol);
 		parameters.put("side", side.toString());
 		parameters.put("type", "MARKET");
-		//parameters.put("timeInForce", "GTC");
 		parameters.put("quoteOrderQty", quoteQty);
 		parameters.put("timestamp", timeStamp);
 		return parameters;
--- src/main/java/com/tradebot/service/Task.java
+++ src/main/java/com/tradebot/service/Task.java
@@ -7,12 +7,21 @@ import com.tradebot.binance.SpotClientConfig;
 import com.tradebot.configuration.OrdersParams;
 import com.tradebot.db.TradeBotDB;
 import com.tradebot.model.TradeBot;
+import java.math.BigDecimal;
+import java.math.RoundingMode;
 import java.time.LocalDateTime;
+import java.util.ArrayList;
 import java.util.Date;
 import java.util.LinkedHashMap;
+import java.util.LinkedHashSet;
+import java.util.List;
+import java.util.ListIterator;
+import java.util.Set;
+import java.util.stream.Collectors;
 import lombok.Data;
 import lombok.Getter;
 import lombok.Setter;
+import org.json.JSONObject;
 
 @Data
 public class Task implements Runnable {
@@ -27,24 +36,76 @@ public class Task implements Runnable {
 		spotClientImpl = SpotClientConfig.spotClientSignTest();
 	}
 	
-	private TradeBot currentBot;
+	Set<BigDecimal> positions = new LinkedHashSet<>();
 	
 	@Override
 	public void run() {
 		
 		//long timeStamp = System.currentTimeMillis();
 		
-//		System.out.println("Bot: " + tradeBot.getTaskId() + " " + LocalDateTime.now());
-//		System.out.println("Query bot 3:  " + currentBot);
-//		System.out.println("------------------------------------------");
-		
-		
-		LinkedHashMap<String, Object> parameters = new LinkedHashMap<>();
-		
 		try {
 			//String result = spotClientImpl.createTrade().newOrder(OrdersParams.getParams(tradeBot.getSymbol(), OrderSide.BUY, tradeBot.getQuoteOrderQty(), timeStamp));
-			String result = spotClientImpl.createMarket().tickerSymbol(OrdersParams.getTickerSymbolParams("BTCUSDT"));
-			System.out.println(result);
+			String result = spotClientImpl.createMarket().tickerSymbol(OrdersParams.getTickerSymbolParams(tradeBot.getSymbol()));
+			
+			JSONObject jsonObject = new JSONObject(result);
+			
+			BigDecimal newPosition = new BigDecimal(jsonObject.getString("price"));
+			
+			System.out.println("Checking the price for " + tradeBot.getSymbol() + " at the price of: " + newPosition.setScale(2, RoundingMode.DOWN));
+			
+			if(positions.isEmpty()) {
+				positions.add(newPosition);
+				System.out.println("First position added for " + tradeBot.getSymbol() + " added at the price: " + newPosition.setScale(2, RoundingMode.DOWN));
+				return;
+			}
+			
+			BigDecimal lastAddedPosition = null;
+			
+			for (BigDecimal element : positions) {
+				lastAddedPosition = element;
+			}
+			
+			int comparison = newPosition.compareTo(lastAddedPosition);
+			BigDecimal positionPercentage = lastAddedPosition.multiply(new BigDecimal(tradeBot.getOrderStep()/100));
+			
+			if (comparison < 0 && (positions.size() <= tradeBot.getCycleMaxOrders())) {
+				BigDecimal decreasedPosition = lastAddedPosition.subtract(positionPercentage);
+				int comparisonDecreaseed = newPosition.compareTo(decreasedPosition);
+				if(comparisonDecreaseed < 0) {						
+					System.out.println("---------------------------");
+					System.out.println("BUYING!");
+					System.out.println("Old Price: " + lastAddedPosition.setScale(2, RoundingMode.DOWN));
+					System.out.println("New Price: " + newPosition.setScale(2, RoundingMode.DOWN));												
+					positions.add(newPosition);
+					System.out.println(tradeBot.getSymbol() + " Position it added, Set size is now: " + positions.size());
+				}					
+			}
+			
+			if (comparison > 0 && !positions.isEmpty()) {
+				BigDecimal increasedPosition = lastAddedPosition.add(positionPercentage);
+				int comparisonIncreased = newPosition.compareTo(increasedPosition);
+				
+				if (comparisonIncreased > 0) {
+					System.out.println("-- In multiple check --");
+					List<BigDecimal> list = positions.stream().collect(Collectors.toList());
+					ListIterator<BigDecimal> iterator = list.listIterator(list.size()); // reverse order
+					while (iterator.hasPrevious()) {
+						BigDecimal previousPosition = iterator.previous();
+						BigDecimal increasedPreviousPosition = previousPosition.add(positionPercentage);						
+						int comparisonIncreasedPosition = newPosition.compareTo(increasedPreviousPosition);
+						if (comparisonIncreasedPosition > 0) {
+							System.out.println("---------------------------");
+							System.out.println("SELLING!");
+							System.out.println("Old Price: " + previousPosition.setScale(2, RoundingMode.DOWN));
+							System.out.println("New Price: " + newPosition.setScale(2, RoundingMode.DOWN));
+							positions.remove(previousPosition);
+							System.out.println(tradeBot.getSymbol() + " Position it removed, Set size is now: " + positions.size());
+						}
+					}
+				}
+			}
+			
+
 		} catch (BinanceConnectorException e) {
 			System.err.println((String) String.format("fullErrMessage: %s", e.getMessage()));
 		} catch (BinanceClientException e) {
