11ba643e757628eb5f44c1fba55dc760cd37a3c0
crypromander <crypromander@DESKTOP-BGFNC9M>
2023-04-17 17:56:25 +0200

    telegram bot chat corrections

--- src/main/java/com/tradebot/service/TelegramBot.java
+++ src/main/java/com/tradebot/service/TelegramBot.java
@@ -1,31 +1,32 @@
 package com.tradebot.service;
 
 import java.io.IOException;
-import java.io.OutputStream;
+import java.io.OutputStreamWriter;
 import java.net.HttpURLConnection;
 import java.net.URL;
-import java.nio.charset.StandardCharsets;
-import lombok.Data;
 
-@Data
 public class TelegramBot {
+
 	private final String BOT_TOKEN = "6206961432:AAGY8arfIXHBE8uw8g1knAEnxY9_ekBVr0c";
 	private final String CHAT_ID = "5638093694";
 
 	public void sendMessage(String message) throws IOException {
-		String urlString = "https://api.telegram.org/bot" + BOT_TOKEN + "/sendMessage";
-		URL url = new URL(urlString);
-		HttpURLConnection con = (HttpURLConnection) url.openConnection();
-		con.setRequestMethod("POST");
-		con.setRequestProperty("Content-Type", "application/json; utf-8");
-		con.setDoOutput(true);
 
-		String jsonInputString = "{\"chat_id\":\"" + CHAT_ID + "\",\"text\":\"" + message + "\"}";
-		try (OutputStream os = con.getOutputStream()) {
-			byte[] input = jsonInputString.getBytes(StandardCharsets.UTF_8);
-			os.write(input, 0, input.length);
-		}
-		con.getInputStream();
-		con.disconnect();
+                URL url = new URL("https://api.telegram.org/bot" + BOT_TOKEN + "/sendMessage");
+                HttpURLConnection conn = (HttpURLConnection) url.openConnection();
+                conn.setRequestMethod("POST");
+                conn.setRequestProperty("Content-Type", "application/json");
+                conn.setDoOutput(true);
+
+                String jsonPayload = "{\"chat_id\":\"" + CHAT_ID + "\",\"text\":\"" + message + "\"}";
+                OutputStreamWriter writer = new OutputStreamWriter(conn.getOutputStream());
+                writer.write(jsonPayload);
+                writer.flush();
+                writer.close();
+
+                if (conn.getResponseCode() != HttpURLConnection.HTTP_OK) {
+                    throw new RuntimeException("Telegram HTTP error code : " + conn.getResponseCode());
+                }
+                conn.disconnect();
 	}
 }
