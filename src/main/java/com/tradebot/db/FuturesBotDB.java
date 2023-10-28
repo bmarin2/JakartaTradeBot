b2c81f556316273f09bca7c8dc4afdbb692fedca
john <john@john-unix>
2023-10-28 19:31:59 +0200

    add futuresBot db class

---
+++ src/main/java/com/tradebot/db/FuturesBotDB.java
@@ -0,0 +1,194 @@
+package com.tradebot.db;
+
+import com.tradebot.model.FuturesBot;
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.sql.Statement;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.concurrent.TimeUnit;
+
+public class FuturesBotDB {
+	
+	public static long addFuturesBot(FuturesBot bot) throws Exception {
+		ConnectionPool pool = ConnectionPool.getInstance();
+		Connection connection = pool.getConnection();
+		PreparedStatement ps = null;
+		ResultSet rs = null;
+		long order_id = 0;
+
+		String query = "INSERT INTO TRADE_BOT (symbol, createdDate, taskId, quantity, description, initialDelay, delay, timeUnit, stopLoss) "
+			+ "VALUES (?, ?, ?, ?, ?, ?, ?, ?, ?)";
+		try {
+			ps = connection.prepareStatement(query, Statement.RETURN_GENERATED_KEYS);
+			
+			ps.setString(1, bot.getSymbol());
+			ps.setDate(2, new java.sql.Date(bot.getCreatedDate().getTime()));
+			ps.setString(3, bot.getTaskId());
+			ps.setDouble(4, bot.getQuantity());
+			ps.setString(5, bot.getDescription());
+			ps.setInt(6, bot.getInitialDelay());
+			ps.setInt(7, bot.getDelay());
+			ps.setInt(8, bot.getTimeUnit().ordinal());
+			ps.setDouble(9, bot.getStopLoss());
+			
+			ps.executeUpdate();
+			
+			rs = ps.getGeneratedKeys();
+
+			if (rs.next()) {
+				order_id = rs.getLong(1);
+			}
+			
+		} catch (SQLException e) {
+			System.err.println(e);
+		} finally {
+			DBUtil.closeResultSet(rs);
+			DBUtil.closePreparedStatement(ps);
+			pool.freeConnection(connection);
+		}
+		return order_id;
+	}
+	
+	public static void updateFuturesBot(FuturesBot bot) throws Exception {
+		ConnectionPool pool = ConnectionPool.getInstance();
+		Connection connection = pool.getConnection();
+		PreparedStatement ps = null;
+		ResultSet rs = null;
+
+		String query = "UPDATE TRADE_BOT SET symbol=?, createdDate=?, taskId=?, quantity=?, description=?, initialDelay=?, delay=?, timeUnit=?, stopLoss=? WHERE id = ?";
+
+		try {
+			ps = connection.prepareStatement(query);
+			
+			ps.setString(1, bot.getSymbol());
+			ps.setDate(2, new java.sql.Date(bot.getCreatedDate().getTime()));
+			ps.setString(3, bot.getTaskId());
+			ps.setDouble(4, bot.getQuantity());
+			ps.setString(5, bot.getDescription());
+			ps.setInt(6, bot.getInitialDelay());
+			ps.setInt(7, bot.getDelay());
+			ps.setInt(8, bot.getTimeUnit().ordinal());
+			ps.setDouble(9, bot.getStopLoss());
+			ps.setLong(10, bot.getId());
+			
+			ps.executeUpdate();
+
+		} catch (SQLException e) {
+			System.err.println(e);
+		} finally {
+			DBUtil.closeResultSet(rs);
+			DBUtil.closePreparedStatement(ps);
+			pool.freeConnection(connection);
+		}
+	}
+	
+	public static List<FuturesBot> getAllFuturesBots() throws Exception {
+		ConnectionPool pool = ConnectionPool.getInstance();
+		Connection connection = pool.getConnection();
+		PreparedStatement ps = null;
+		ResultSet rs = null;
+
+		String query = "SELECT * FROM FUTURES_BOT";
+		try {
+			ps = connection.prepareStatement(query);
+			rs = ps.executeQuery();
+			List<FuturesBot> bots = new ArrayList<>();
+			while (rs.next()) {
+				FuturesBot bot = new FuturesBot();
+				bot.setId(rs.getLong("id"));
+				bot.setSymbol(rs.getString("symbol"));
+				bot.setCreatedDate(rs.getDate("createdDate"));
+				bot.setTaskId(rs.getString("taskId"));
+				bot.setQuantity(rs.getDouble("quantity"));
+				bot.setDescription(rs.getString("description"));
+				bot.setInitialDelay(rs.getInt("initialDelay"));
+				bot.setDelay(rs.getInt("delay"));
+				bot.setTimeUnit(TimeUnit.values()[rs.getInt("timeUnit")]);
+				bot.setStopLoss(rs.getDouble("stopLoss"));
+				bots.add(bot);
+			}
+			return bots;
+		} catch (SQLException e) {
+			System.err.println(e);
+			return null;
+		} finally {
+			DBUtil.closeResultSet(rs);
+			DBUtil.closePreparedStatement(ps);
+			pool.freeConnection(connection);
+		}
+	}
+
+	public static FuturesBot getOneFuturesBot(long id) throws Exception {
+		ConnectionPool pool = ConnectionPool.getInstance();
+		Connection connection = pool.getConnection();
+		PreparedStatement ps = null;
+		ResultSet rs = null;
+
+		String query = "SELECT * FROM FUTURES_BOT where id=?";
+		try {
+			ps = connection.prepareStatement(query);
+			ps.setLong(1, id);
+			rs = ps.executeQuery();
+			FuturesBot bot = new FuturesBot();
+			while (rs.next()) {
+				bot.setId(rs.getLong("id"));
+				bot.setSymbol(rs.getString("symbol"));
+				bot.setCreatedDate(rs.getDate("createdDate"));
+				bot.setTaskId(rs.getString("taskId"));
+				bot.setQuantity(rs.getDouble("quantity"));
+				bot.setDescription(rs.getString("description"));
+				bot.setInitialDelay(rs.getInt("initialDelay"));
+				bot.setDelay(rs.getInt("delay"));
+				bot.setTimeUnit(TimeUnit.values()[rs.getInt("timeUnit")]);
+				bot.setStopLoss(rs.getDouble("stopLoss"));
+			}
+			return bot;
+		} catch (SQLException e) {
+			System.err.println(e);
+			return null;
+		} finally {
+			DBUtil.closeResultSet(rs);
+			DBUtil.closePreparedStatement(ps);
+			pool.freeConnection(connection);
+		}
+	}
+	
+	public static FuturesBot getOneTradeBot(String taskId) throws Exception {
+		ConnectionPool pool = ConnectionPool.getInstance();
+		Connection connection = pool.getConnection();
+		PreparedStatement ps = null;
+		ResultSet rs = null;
+
+		String query = "SELECT * FROM FUTURES_BOT where taskId=?";
+		try {
+			ps = connection.prepareStatement(query);
+			ps.setString(1, taskId);
+			rs = ps.executeQuery();
+			FuturesBot bot = new FuturesBot();
+			while (rs.next()) {
+				bot.setId(rs.getLong("id"));
+				bot.setSymbol(rs.getString("symbol"));
+				bot.setCreatedDate(rs.getDate("createdDate"));
+				bot.setTaskId(rs.getString("taskId"));
+				bot.setQuantity(rs.getDouble("quantity"));
+				bot.setDescription(rs.getString("description"));
+				bot.setInitialDelay(rs.getInt("initialDelay"));
+				bot.setDelay(rs.getInt("delay"));
+				bot.setTimeUnit(TimeUnit.values()[rs.getInt("timeUnit")]);
+				bot.setStopLoss(rs.getDouble("stopLoss"));
+			}
+			return bot;
+		} catch (SQLException e) {
+			System.err.println(e);
+			return null;
+		} finally {
+			DBUtil.closeResultSet(rs);
+			DBUtil.closePreparedStatement(ps);
+			pool.freeConnection(connection);
+		}
+	}
+	
+}
