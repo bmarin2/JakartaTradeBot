f8d8dbe8df48470e31bdf6ce062d45baeec64502
john <john@john-unix>
2023-11-01 20:26:53 +0100

    add futures config

--- src/main/java/com/tradebot/binance/UMFuturesClientConfig.java
+++ src/main/java/com/tradebot/binance/UMFuturesClientConfig.java
@@ -1,10 +1,26 @@
 package com.tradebot.binance;
 
 import com.binance.connector.futures.client.impl.UMFuturesClientImpl;
+import com.tradebot.configuration.ConfigParams;
 
 public class UMFuturesClientConfig {
 		
-	public static UMFuturesClientImpl futuresClientOnlyBaseURLProd() {
+	public static UMFuturesClientImpl futuresBaseURLProd() {
 		return new UMFuturesClientImpl();
+	}
+	
+	public static UMFuturesClientImpl futuresSignedProd() {
+		return new UMFuturesClientImpl(ConfigParams.F_API_KEY, 
+								 ConfigParams.F_SECRET_KEY);
+	}
+	
+	public static UMFuturesClientImpl futuresBaseURLTest() {
+		return new UMFuturesClientImpl(ConfigParams.F_TESTNET_BASE_URL);
+	}
+	
+	public static UMFuturesClientImpl futuresSignedTest() {
+		return new UMFuturesClientImpl(ConfigParams.F_TESTNET_API_KEY,
+								 ConfigParams.F_TESTNET_SECRET_KEY,
+								 ConfigParams.F_TESTNET_BASE_URL);
 	}
 }
