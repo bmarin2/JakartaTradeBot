6e7ddd3ca39481264135e3bbf73d6e655b07ee9c
john <john@nathan-pc>
2024-12-16 14:20:16 +0100

    add backtest data

---
+++ src/main/java/com/tradebot/data/BarsBNBUSDT.java
@@ -0,0 +1,148 @@
+package com.tradebot.data;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class BarsBNBUSDT {
+	public static List<String> getBars_5m_OKT_2024() {
+		List<String> files = new ArrayList<>();
+		files.add(System.getProperty("user.home") + "/output-bars/bnb/2024/5m/okt/" + "bars-1-10-2024.json");
+		files.add(System.getProperty("user.home") + "/output-bars/bnb/2024/5m/okt/" + "bars-2-10-2024.json");
+		files.add(System.getProperty("user.home") + "/output-bars/bnb/2024/5m/okt/" + "bars-3-10-2024.json");
+		files.add(System.getProperty("user.home") + "/output-bars/bnb/2024/5m/okt/" + "bars-4-10-2024.json");
+		files.add(System.getProperty("user.home") + "/output-bars/bnb/2024/5m/okt/" + "bars-5-10-2024.json");
+		files.add(System.getProperty("user.home") + "/output-bars/bnb/2024/5m/okt/" + "bars-6-10-2024.json");
+		files.add(System.getProperty("user.home") + "/output-bars/bnb/2024/5m/okt/" + "bars-7-10-2024.json");
+		files.add(System.getProperty("user.home") + "/output-bars/bnb/2024/5m/okt/" + "bars-8-10-2024.json");
+		files.add(System.getProperty("user.home") + "/output-bars/bnb/2024/5m/okt/" + "bars-9-10-2024.json");
+		files.add(System.getProperty("user.home") + "/output-bars/bnb/2024/5m/okt/" + "bars-10-10-2024.json");
+		files.add(System.getProperty("user.home") + "/output-bars/bnb/2024/5m/okt/" + "bars-11-10-2024.json");
+		files.add(System.getProperty("user.home") + "/output-bars/bnb/2024/5m/okt/" + "bars-12-10-2024.json");
+		files.add(System.getProperty("user.home") + "/output-bars/bnb/2024/5m/okt/" + "bars-13-10-2024.json");
+		files.add(System.getProperty("user.home") + "/output-bars/bnb/2024/5m/okt/" + "bars-14-10-2024.json");
+		files.add(System.getProperty("user.home") + "/output-bars/bnb/2024/5m/okt/" + "bars-15-10-2024.json");
+		files.add(System.getProperty("user.home") + "/output-bars/bnb/2024/5m/okt/" + "bars-16-10-2024.json");
+		files.add(System.getProperty("user.home") + "/output-bars/bnb/2024/5m/okt/" + "bars-17-10-2024.json");
+		files.add(System.getProperty("user.home") + "/output-bars/bnb/2024/5m/okt/" + "bars-18-10-2024.json");
+		files.add(System.getProperty("user.home") + "/output-bars/bnb/2024/5m/okt/" + "bars-19-10-2024.json");
+		files.add(System.getProperty("user.home") + "/output-bars/bnb/2024/5m/okt/" + "bars-20-10-2024.json");
+		files.add(System.getProperty("user.home") + "/output-bars/bnb/2024/5m/okt/" + "bars-21-10-2024.json");
+		files.add(System.getProperty("user.home") + "/output-bars/bnb/2024/5m/okt/" + "bars-22-10-2024.json");
+		files.add(System.getProperty("user.home") + "/output-bars/bnb/2024/5m/okt/" + "bars-23-10-2024.json");
+		files.add(System.getProperty("user.home") + "/output-bars/bnb/2024/5m/okt/" + "bars-24-10-2024.json");
+		files.add(System.getProperty("user.home") + "/output-bars/bnb/2024/5m/okt/" + "bars-25-10-2024.json");
+		files.add(System.getProperty("user.home") + "/output-bars/bnb/2024/5m/okt/" + "bars-26-10-2024.json");
+		files.add(System.getProperty("user.home") + "/output-bars/bnb/2024/5m/okt/" + "bars-27-10-2024.json");
+		files.add(System.getProperty("user.home") + "/output-bars/bnb/2024/5m/okt/" + "bars-28-10-2024.json");
+		files.add(System.getProperty("user.home") + "/output-bars/bnb/2024/5m/okt/" + "bars-29-10-2024.json");
+		files.add(System.getProperty("user.home") + "/output-bars/bnb/2024/5m/okt/" + "bars-30-10-2024.json");
+		files.add(System.getProperty("user.home") + "/output-bars/bnb/2024/5m/okt/" + "bars-31-10-2024.json");
+		return files;
+	}
+	
+	public static List<String> getBars_1h_OKT_2024() {
+		List<String> files = new ArrayList<>();
+		files.add(System.getProperty("user.home") + "/output-bars/bnb/2024/1h/okt/" + "bars-1-10-2024.json");
+		files.add(System.getProperty("user.home") + "/output-bars/bnb/2024/1h/okt/" + "bars-2-10-2024.json");
+		files.add(System.getProperty("user.home") + "/output-bars/bnb/2024/1h/okt/" + "bars-3-10-2024.json");
+		files.add(System.getProperty("user.home") + "/output-bars/bnb/2024/1h/okt/" + "bars-4-10-2024.json");
+		files.add(System.getProperty("user.home") + "/output-bars/bnb/2024/1h/okt/" + "bars-5-10-2024.json");
+		files.add(System.getProperty("user.home") + "/output-bars/bnb/2024/1h/okt/" + "bars-6-10-2024.json");
+		files.add(System.getProperty("user.home") + "/output-bars/bnb/2024/1h/okt/" + "bars-7-10-2024.json");
+		files.add(System.getProperty("user.home") + "/output-bars/bnb/2024/1h/okt/" + "bars-8-10-2024.json");
+		files.add(System.getProperty("user.home") + "/output-bars/bnb/2024/1h/okt/" + "bars-9-10-2024.json");
+		files.add(System.getProperty("user.home") + "/output-bars/bnb/2024/1h/okt/" + "bars-10-10-2024.json");
+		files.add(System.getProperty("user.home") + "/output-bars/bnb/2024/1h/okt/" + "bars-11-10-2024.json");
+		files.add(System.getProperty("user.home") + "/output-bars/bnb/2024/1h/okt/" + "bars-12-10-2024.json");
+		files.add(System.getProperty("user.home") + "/output-bars/bnb/2024/1h/okt/" + "bars-13-10-2024.json");
+		files.add(System.getProperty("user.home") + "/output-bars/bnb/2024/1h/okt/" + "bars-14-10-2024.json");
+		files.add(System.getProperty("user.home") + "/output-bars/bnb/2024/1h/okt/" + "bars-15-10-2024.json");
+		files.add(System.getProperty("user.home") + "/output-bars/bnb/2024/1h/okt/" + "bars-16-10-2024.json");
+		files.add(System.getProperty("user.home") + "/output-bars/bnb/2024/1h/okt/" + "bars-17-10-2024.json");
+		files.add(System.getProperty("user.home") + "/output-bars/bnb/2024/1h/okt/" + "bars-18-10-2024.json");
+		files.add(System.getProperty("user.home") + "/output-bars/bnb/2024/1h/okt/" + "bars-19-10-2024.json");
+		files.add(System.getProperty("user.home") + "/output-bars/bnb/2024/1h/okt/" + "bars-20-10-2024.json");
+		files.add(System.getProperty("user.home") + "/output-bars/bnb/2024/1h/okt/" + "bars-21-10-2024.json");
+		files.add(System.getProperty("user.home") + "/output-bars/bnb/2024/1h/okt/" + "bars-22-10-2024.json");
+		files.add(System.getProperty("user.home") + "/output-bars/bnb/2024/1h/okt/" + "bars-23-10-2024.json");
+		files.add(System.getProperty("user.home") + "/output-bars/bnb/2024/1h/okt/" + "bars-24-10-2024.json");
+		files.add(System.getProperty("user.home") + "/output-bars/bnb/2024/1h/okt/" + "bars-25-10-2024.json");
+		files.add(System.getProperty("user.home") + "/output-bars/bnb/2024/1h/okt/" + "bars-26-10-2024.json");
+		files.add(System.getProperty("user.home") + "/output-bars/bnb/2024/1h/okt/" + "bars-27-10-2024.json");
+		files.add(System.getProperty("user.home") + "/output-bars/bnb/2024/1h/okt/" + "bars-28-10-2024.json");
+		files.add(System.getProperty("user.home") + "/output-bars/bnb/2024/1h/okt/" + "bars-29-10-2024.json");
+		files.add(System.getProperty("user.home") + "/output-bars/bnb/2024/1h/okt/" + "bars-30-10-2024.json");
+		files.add(System.getProperty("user.home") + "/output-bars/bnb/2024/1h/okt/" + "bars-31-10-2024.json");
+		return files;
+	}
+	
+	public static List<String> getBars_5m_NOV_2024() {
+		List<String> files = new ArrayList<>();		
+		files.add(System.getProperty("user.home") + "/output-bars/bnb/2024/5m/nov/" + "bars-1-11-2024.json");
+		files.add(System.getProperty("user.home") + "/output-bars/bnb/2024/5m/nov/" + "bars-2-11-2024.json");
+		files.add(System.getProperty("user.home") + "/output-bars/bnb/2024/5m/nov/" + "bars-3-11-2024.json");
+		files.add(System.getProperty("user.home") + "/output-bars/bnb/2024/5m/nov/" + "bars-4-11-2024.json");
+		files.add(System.getProperty("user.home") + "/output-bars/bnb/2024/5m/nov/" + "bars-5-11-2024.json");
+		files.add(System.getProperty("user.home") + "/output-bars/bnb/2024/5m/nov/" + "bars-6-11-2024.json");
+		files.add(System.getProperty("user.home") + "/output-bars/bnb/2024/5m/nov/" + "bars-7-11-2024.json");
+		files.add(System.getProperty("user.home") + "/output-bars/bnb/2024/5m/nov/" + "bars-8-11-2024.json");
+		files.add(System.getProperty("user.home") + "/output-bars/bnb/2024/5m/nov/" + "bars-9-11-2024.json");
+		files.add(System.getProperty("user.home") + "/output-bars/bnb/2024/5m/nov/" + "bars-10-11-2024.json");
+		files.add(System.getProperty("user.home") + "/output-bars/bnb/2024/5m/nov/" + "bars-11-11-2024.json");
+		files.add(System.getProperty("user.home") + "/output-bars/bnb/2024/5m/nov/" + "bars-12-11-2024.json");
+		files.add(System.getProperty("user.home") + "/output-bars/bnb/2024/5m/nov/" + "bars-13-11-2024.json");
+		files.add(System.getProperty("user.home") + "/output-bars/bnb/2024/5m/nov/" + "bars-14-11-2024.json");
+		files.add(System.getProperty("user.home") + "/output-bars/bnb/2024/5m/nov/" + "bars-15-11-2024.json");
+		files.add(System.getProperty("user.home") + "/output-bars/bnb/2024/5m/nov/" + "bars-16-11-2024.json");
+		files.add(System.getProperty("user.home") + "/output-bars/bnb/2024/5m/nov/" + "bars-17-11-2024.json");
+		files.add(System.getProperty("user.home") + "/output-bars/bnb/2024/5m/nov/" + "bars-18-11-2024.json");
+		files.add(System.getProperty("user.home") + "/output-bars/bnb/2024/5m/nov/" + "bars-19-11-2024.json");
+		files.add(System.getProperty("user.home") + "/output-bars/bnb/2024/5m/nov/" + "bars-20-11-2024.json");
+		files.add(System.getProperty("user.home") + "/output-bars/bnb/2024/5m/nov/" + "bars-21-11-2024.json");
+		files.add(System.getProperty("user.home") + "/output-bars/bnb/2024/5m/nov/" + "bars-22-11-2024.json");
+		files.add(System.getProperty("user.home") + "/output-bars/bnb/2024/5m/nov/" + "bars-23-11-2024.json");
+		files.add(System.getProperty("user.home") + "/output-bars/bnb/2024/5m/nov/" + "bars-24-11-2024.json");
+		files.add(System.getProperty("user.home") + "/output-bars/bnb/2024/5m/nov/" + "bars-25-11-2024.json");
+		files.add(System.getProperty("user.home") + "/output-bars/bnb/2024/5m/nov/" + "bars-26-11-2024.json");
+		files.add(System.getProperty("user.home") + "/output-bars/bnb/2024/5m/nov/" + "bars-27-11-2024.json");
+		files.add(System.getProperty("user.home") + "/output-bars/bnb/2024/5m/nov/" + "bars-28-11-2024.json");
+		files.add(System.getProperty("user.home") + "/output-bars/bnb/2024/5m/nov/" + "bars-29-11-2024.json");
+		files.add(System.getProperty("user.home") + "/output-bars/bnb/2024/5m/nov/" + "bars-30-11-2024.json");
+		return files;
+	}
+	
+	public static List<String> getBars_1h_NOV_2024() {
+		List<String> files = new ArrayList<>();		
+		files.add(System.getProperty("user.home") + "/output-bars/bnb/2024/1h/nov/" + "bars-1-11-2024.json");
+		files.add(System.getProperty("user.home") + "/output-bars/bnb/2024/1h/nov/" + "bars-2-11-2024.json");
+		files.add(System.getProperty("user.home") + "/output-bars/bnb/2024/1h/nov/" + "bars-3-11-2024.json");
+		files.add(System.getProperty("user.home") + "/output-bars/bnb/2024/1h/nov/" + "bars-4-11-2024.json");
+		files.add(System.getProperty("user.home") + "/output-bars/bnb/2024/1h/nov/" + "bars-5-11-2024.json");
+		files.add(System.getProperty("user.home") + "/output-bars/bnb/2024/1h/nov/" + "bars-6-11-2024.json");
+		files.add(System.getProperty("user.home") + "/output-bars/bnb/2024/1h/nov/" + "bars-7-11-2024.json");
+		files.add(System.getProperty("user.home") + "/output-bars/bnb/2024/1h/nov/" + "bars-8-11-2024.json");
+		files.add(System.getProperty("user.home") + "/output-bars/bnb/2024/1h/nov/" + "bars-9-11-2024.json");
+		files.add(System.getProperty("user.home") + "/output-bars/bnb/2024/1h/nov/" + "bars-10-11-2024.json");
+		files.add(System.getProperty("user.home") + "/output-bars/bnb/2024/1h/nov/" + "bars-11-11-2024.json");
+		files.add(System.getProperty("user.home") + "/output-bars/bnb/2024/1h/nov/" + "bars-12-11-2024.json");
+		files.add(System.getProperty("user.home") + "/output-bars/bnb/2024/1h/nov/" + "bars-13-11-2024.json");
+		files.add(System.getProperty("user.home") + "/output-bars/bnb/2024/1h/nov/" + "bars-14-11-2024.json");
+		files.add(System.getProperty("user.home") + "/output-bars/bnb/2024/1h/nov/" + "bars-15-11-2024.json");
+		files.add(System.getProperty("user.home") + "/output-bars/bnb/2024/1h/nov/" + "bars-16-11-2024.json");
+		files.add(System.getProperty("user.home") + "/output-bars/bnb/2024/1h/nov/" + "bars-17-11-2024.json");
+		files.add(System.getProperty("user.home") + "/output-bars/bnb/2024/1h/nov/" + "bars-18-11-2024.json");
+		files.add(System.getProperty("user.home") + "/output-bars/bnb/2024/1h/nov/" + "bars-19-11-2024.json");
+		files.add(System.getProperty("user.home") + "/output-bars/bnb/2024/1h/nov/" + "bars-20-11-2024.json");
+		files.add(System.getProperty("user.home") + "/output-bars/bnb/2024/1h/nov/" + "bars-21-11-2024.json");
+		files.add(System.getProperty("user.home") + "/output-bars/bnb/2024/1h/nov/" + "bars-22-11-2024.json");
+		files.add(System.getProperty("user.home") + "/output-bars/bnb/2024/1h/nov/" + "bars-23-11-2024.json");
+		files.add(System.getProperty("user.home") + "/output-bars/bnb/2024/1h/nov/" + "bars-24-11-2024.json");
+		files.add(System.getProperty("user.home") + "/output-bars/bnb/2024/1h/nov/" + "bars-25-11-2024.json");
+		files.add(System.getProperty("user.home") + "/output-bars/bnb/2024/1h/nov/" + "bars-26-11-2024.json");
+		files.add(System.getProperty("user.home") + "/output-bars/bnb/2024/1h/nov/" + "bars-27-11-2024.json");
+		files.add(System.getProperty("user.home") + "/output-bars/bnb/2024/1h/nov/" + "bars-28-11-2024.json");
+		files.add(System.getProperty("user.home") + "/output-bars/bnb/2024/1h/nov/" + "bars-29-11-2024.json");
+		files.add(System.getProperty("user.home") + "/output-bars/bnb/2024/1h/nov/" + "bars-30-11-2024.json");
+		return files;
+	}
+}
